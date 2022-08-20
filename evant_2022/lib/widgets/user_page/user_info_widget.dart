253fba64f5408a684b27cf37c47baa8dc734b6ed
Soun Sean Kim <sukim2406@gmail.com>
2022-08-20 11:53:50 +0900

    Evant_2022 : User Page Profile Picture ::::: daily commit project 08-20-2022

--- evant_2022/lib/widgets/user_page/user_info_widget.dart
+++ evant_2022/lib/widgets/user_page/user_info_widget.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import '../../widgets/responsive_layout_widget.dart';
 
+import '../../controllers/global_controller.dart' as global;
+
 class UserInfoWidget extends StatefulWidget {
   final Map userDoc;
   const UserInfoWidget({
@@ -42,7 +44,29 @@ class _UserInfoMobileWidgetState extends State<UserInfoMobileWidget> {
     return Container(
       height: MediaQuery.of(context).size.height * .75,
       width: MediaQuery.of(context).size.width * .9,
-      color: Colors.red,
+      decoration: BoxDecoration(
+        color: global.secondaryColor,
+        border: Border.all(
+          color: global.secondaryColor,
+          width: 5,
+        ),
+        borderRadius: const BorderRadius.only(
+          bottomLeft: Radius.circular(20),
+          bottomRight: Radius.circular(20),
+        ),
+      ),
+      child: Column(
+        children: [
+          CircleAvatar(
+            radius: MediaQuery.of(context).size.height * .1,
+            backgroundColor: global.secondaryColor,
+            backgroundImage: Image.network(
+              widget.userDoc['profilePicture'],
+              fit: BoxFit.contain,
+            ).image,
+          ),
+        ],
+      ),
     );
   }
 }
